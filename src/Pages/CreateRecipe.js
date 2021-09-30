395d2e0bc443e9124e022ec2e60e97cb29d24fd0
Steven <stevencheung@hotmail.co.uk>
2021-09-30 22:25:42 +0100

    add ingredients

--- src/Pages/CreateRecipe.js
+++ src/Pages/CreateRecipe.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect, useContext } from "react";
 import { GlobalContext } from "../context";
 import "./CreateRecipe.css";
-import { Link } from "@reach/router";
+import { Link, navigate } from "@reach/router";
 
 function CreateRecipe() {
   const { state, dispatch } = useContext(GlobalContext);
   const { recipes } = state;
   const [name, setName] = useState();
   const [method, setMethod] = useState();
+  const [ingredients, setIngredients] = useState([]);
+  const [ingredientsName, setIngredientsName] = useState();
+  const [ingredientsUnit, setIngredientsUnit] = useState();
+  const [ingredientsMeasure, setIngredientsMeasure] = useState();
 
   const onNameChange = (e) => {
     setName(e.target.value);
@@ -17,15 +21,46 @@ function CreateRecipe() {
     setMethod(e.target.value);
   };
 
+  const onSaveIngredients = (e) => {
+    // 1. create ingredients object
+
+    const NewIngredient = {
+      measurement: ingredientsMeasure,
+      unit: ingredientsUnit,
+      name: ingredientsName,
+    };
+
+    // 2. method to add object into the array
+    const updatedIngredients = [...ingredients, NewIngredient];
+
+    // 3. save the state
+    setIngredients(updatedIngredients);
+
+    // 4. display ingredient on screen
+
+    // 5. clear all box
+    setIngredientsName("");
+    setIngredientsUnit("");
+    setIngredientsMeasure("");
+  };
+
   const onSave = () => {
     const newRecipe = {
       id: recipes[recipes.length - 1].id + 1,
       name: name,
       method: method,
-      ingredients: [],
+      ingredients: ingredients,
     };
     const updatedRecipes = [...recipes, newRecipe];
     dispatch({ type: "SET_RECIPES", recipes: updatedRecipes });
+    navigate(`/`);
+  };
+
+  const onDeleteIngredient = (index) => {
+    const updatedIngredients = ingredients.filter(
+      (ingredient, idx) => idx !== index
+    );
+    setIngredients(updatedIngredients);
   };
 
   return (
@@ -40,6 +75,40 @@ function CreateRecipe() {
         <div>
           <p>Method:</p>
           <input type="text" onChange={onMethodChange}></input>
+        </div>
+        <p>Ingredients measurement:</p>
+
+        <input
+          type="text"
+          value={ingredientsMeasure}
+          onChange={(e) => setIngredientsMeasure(e.target.value)}
+        ></input>
+        <div>
+          <p>Ingredients unit:</p>
+
+          <input
+            type="text"
+            value={ingredientsUnit}
+            onChange={(e) => setIngredientsUnit(e.target.value)}
+          ></input>
+
+          <p>Ingredients name:</p>
+          <input
+            type="text"
+            value={ingredientsName}
+            onChange={(e) => setIngredientsName(e.target.value)}
+          ></input>
+          <div>
+            <button onClick={onSaveIngredients}>Add ingredient</button>
+          </div>
+          <div>
+            {ingredients.length > 0 &&
+              ingredients.map((ingredient, index) => (
+                <div onClick={() => onDeleteIngredient(index)}>
+                  {ingredient.measurement} {ingredient.unit} {ingredient.name}
+                </div>
+              ))}
+          </div>
         </div>
         <button onClick={onSave}>Save</button>
       </div>
